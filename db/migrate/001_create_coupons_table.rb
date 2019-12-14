48c20522eba187e81656245a6b21b3043e38aef0
Cristal Alatorre Perez <cristalalatorreperez@Albert.local>
2019-12-14 14:15:16 -0500

    Done.

--- db/migrate/001_create_coupons_table.rb
+++ db/migrate/001_create_coupons_table.rb
@@ -8,3 +8,15 @@ class CreateCouponsTable < ActiveRecord::Migration
    end
 
 end
+
+#solution code:
+# class CreateCoupons < ActiveRecord::Migration
+#   def change
+#     create_table :coupons do |t|
+#       t.string :coupon_code
+#       t.string :store
+#
+#       t.timestamps null: false
+#     end
+#   end
+# end
